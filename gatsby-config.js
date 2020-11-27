8e8eb0fc728fab5854343b8a3426238a3fc1b058
Yasuharu Sato <yasufabric1192@gmail.com>
2020-11-27 18:01:37 +0900

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Yasufabric Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `yasufabric`,
+      summary: `who lives and works in Hokkaido building useful things.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    description: `A blog demonstrating what yasufabric can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
     social: {
       twitter: `kylemathews`,
